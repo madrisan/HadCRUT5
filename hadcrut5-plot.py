99fc0c6f122eb850065432cbddc701d387a70b16
Davide Madrisan <davide.madrisan@gmail.com>
2021-01-03 12:16:17 +0100

    Add a command-line flag for saving the plot as a png file
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- hadcrut5-plot.py
+++ hadcrut5-plot.py
@@ -4,13 +4,50 @@
 # Copyright (c) 2020 Davide Madrisan <davide.madrisan@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import argparse
 import json
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import netCDF4 as nc
 import os
 import requests
-import yaml
+
+__author__ = "Davide Madrisan"
+__copyright__ = "Copyright (C) 2020 Davide Madrisan"
+__license__ = "Apache License 2.0 (Apache-2.0)"
+__version__ = "1"
+__email__ = "davide.madrisan@gmail.com"
+__status__ = "beta"
+
+def copyleft(descr):
+    """Print the Copyright message and License """
+    return ("{} v.{} ({})\n{} <{}>\nLicense: {}"
+            .format(
+                descr, __version__, __status__,
+                __copyright__, __email__,
+                __license__))
+
+def argparser(descr, examples):
+    """Return a new ArgumentParser object """
+    return argparse.ArgumentParser(
+               formatter_class = argparse.RawDescriptionHelpFormatter,
+               description = copyleft(descr),
+               epilog = "examples:\n  " + "\n  ".join(examples))
+
+def parse_args():
+    """This function parses and return arguments passed in """
+    descr = "Parse and plot the HadCRUT5 temperature datasets"
+    examples = [
+       "%(prog)s",
+       "%(prog)s --outfile HadCRUT5.png"]
+
+    parser = argparser(descr, examples)
+    parser.add_argument(
+        "-f", "--outfile",
+        action="store", dest="outfile",
+        help="name of the output PNG file")
+
+    return parser.parse_args()
 
 # You can dump a NetCFG file using the command:
 # $ ncdump -h <ncfile>
@@ -42,18 +79,8 @@ def dataset_load(dataset_filename):
 
     return (metadata, dimensions, variables)
 
-def main():
-    datasets = {
-        "Global":
-             "HadCRUT.5.0.0.0.analysis.summary_series.global.annual.nc",
-        "Northern Hemisphere":
-            "HadCRUT.5.0.0.0.analysis.summary_series.northern_hemisphere.annual.nc",
-        "Southern Hemisphere":
-            "HadCRUT.5.0.0.0.analysis.summary_series.southern_hemisphere.annual.nc"
-    }
-
+def plot(datasets, outfile):
     mpl.style.use("seaborn-notebook")
-    #mpl.style.use("seaborn-muted")
 
     for item in datasets:
         metadata, dimensions, variables = dataset_load(datasets[item])
@@ -70,9 +97,23 @@ def main():
     plt.axvline(x=2021, color="lightgray", label="2021", linewidth=1,
                 linestyle="dotted")
 
-    plt.savefig("HadCRUT5.png", transparent=False)
+    if outfile:
+        plt.savefig(outfile, transparent=False)
 
     plt.show()
+
+def main():
+    datasets = {
+        "Global":
+             "HadCRUT.5.0.0.0.analysis.summary_series.global.annual.nc",
+        "Northern Hemisphere":
+            "HadCRUT.5.0.0.0.analysis.summary_series.northern_hemisphere.annual.nc",
+        "Southern Hemisphere":
+            "HadCRUT.5.0.0.0.analysis.summary_series.southern_hemisphere.annual.nc"
+    }
+
+    args = parse_args()
+    plot(datasets, args.outfile)
 
 if __name__ == "__main__":
     main()
