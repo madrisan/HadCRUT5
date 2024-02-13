77e9a4b34b8871bae8a561eb74146e4d2a09e824
Davide Madrisan <d.madrisan@proton.me>
2024-02-13 21:23:54 +0100

    feat: make plot.sh a bit more verbose
    Signed-off-by: Davide Madrisan <d.madrisan@proton.me>

--- plots.sh
+++ plots.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+echo "removing .nc files ..."
 rm -f HadCRUT.*.nc
 
 for period in "1961-1990" "1850-1900" "1880-1920"; do
+    echo "creating plot for period $period ..."
     ./hadcrut5-plot.py \
         --period="${period}" \
         --annotate=2 \
@@ -16,12 +18,16 @@ for period in "1961-1990" "1850-1900" "1880-1920"; do
         --outfile plots/HadCRUT5-global-${period}.png
 done
 
+echo "creating monthly plot ..."
 ./hadcrut5-plot.py \
     --period "1880-1920" \
     --time-series monthly \
     --global \
     --outfile plots/HadCRUT5-monthly-global-1880-1920.png
 
+echo "creating stripe image ..."
 ./hadcrut5-stripe.py \
     --region global \
     --outfile plots/HadCRUT5-global-stripe.png
+
+echo "done."
